b3db8f4308b11f5a966e314326b08018e0135313
Adam <adam.rida1998@hotmail.fr>
2023-12-22 14:02:16 +0100

    Fix rag benchmark file imports

--- huggingface_space_app/rag_benchmark.py
+++ huggingface_space_app/rag_benchmark.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 from temporal_augmented_retrival import get_answer as get_temporal_answer
-from retrieval_classic_retrieval import get_answer as get_rag_answer
-from retrieval_naive import get_answer as get_naive_answer
+from rag import get_answer as get_rag_answer
+from naive_rag import get_answer as get_naive_answer
 
-path_to_csv = "../contenu_embedded_august2023_1.csv"
+path_to_csv = "contenu_embedded_august2023_1.csv"
 path_to_raw = "stockerbot-export.csv"
 df = pd.read_csv(path_to_csv, on_bad_lines='skip').reset_index(drop=True).drop(columns=['Unnamed: 0'])
 df["embedding"] = df.embedding.apply(lambda x: eval(x)).to_list()
